75675120ec064a1143d27f53be723f9d37521b5c
Anton Krylov <anthony.kryloff@gmail.com>
2016-12-02 23:12:43 +0300

    update Kotanchiki

--- src/kotanchiki/Main.java
+++ src/kotanchiki/Main.java
@@ -24,36 +24,36 @@ public class Main extends Application {
     public static final double HERO_SIZE = 100;
     public static final double SCREEN_SIZE = 1920;
 
-    private AudioClip china = new AudioClip(getClass().getResource("/bigchina.mp3").toExternalForm());
+    private AudioClip chinaClip = new AudioClip(getClass().getResource("/bigchina.mp3").toExternalForm());
+    private Image trumpOpen = new Image(getClass().getResource("/trump_open1.png").toString());
+    private Image trumpClosed = new Image(getClass().getResource("/trump_close1.png").toString());
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         BorderPane root = new BorderPane();
 
-        Image open = new Image(getClass().getResource("/trump_open1.png").toString());
-        Image closed = new Image(getClass().getResource("/trump_close1.png").toURI().toURL().toString());
-
-        ImageView iv = new ImageView(closed);
+        ImageView iv = new ImageView(trumpClosed);
         iv.setFitWidth(HERO_SIZE);
         iv.setFitHeight(HERO_SIZE);
 
         root.getChildren().add(iv);
 
-        BulletTimer timer = new BulletTimer();
+        BulletTimer timer = new BulletTimer(root);
         timer.start();
 
         root.setOnKeyPressed(event -> {
                 KeyCode code = event.getCode();
                 if(code.equals(KeyCode.SPACE)) {
-                    iv.setImage(open);
+                    iv.setImage(trumpOpen);
 
                     double y = iv.getY()+HERO_SIZE/2;
-                    double x = iv.getX()+HERO_SIZE/2-Bullet.SIZE/2 + 70*iv.getScaleX();
+                    double x = iv.getX()+HERO_SIZE/2- BulletView.SIZE/2 + 70*iv.getScaleX();
 
-                    Bullet bullet = new Bullet(x, y, iv.getScaleX() == 1, root);
+                    BulletView bullet = new BulletView(x, y, iv.getScaleX() == 1);
+                    root.getChildren().add(bullet);
                     timer.addBullet(bullet);
 
-                    china.play();
+                    chinaClip.play();
                 } else if(code.equals(KeyCode.UP)) {
                     iv.setY(iv.getY()-10);
                 } else if(code.equals(KeyCode.DOWN)) {
@@ -69,13 +69,13 @@ public class Main extends Application {
 
         root.setOnKeyReleased(event -> {
             if(event.getCode().equals(KeyCode.SPACE)) {
-                iv.setImage(closed);
+                iv.setImage(trumpClosed);
             }
         });
 
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
-        primaryStage.getScene().getRoot().requestFocus();
+        root.requestFocus();
     }
 
     public static void main(String[] args) {
@@ -85,80 +85,60 @@ public class Main extends Application {
 
 class BulletTimer extends AnimationTimer {
 
-    private List<Bullet> bullets = new ArrayList<>();
+    private List<BulletView> bullets = new ArrayList<>();
+    private Pane pane;
 
     private long was = System.nanoTime();
+
+    public BulletTimer(Pane pane) {
+        this.pane = pane;
+    }
 
     @Override
     public void handle(long now) {
         long delta = now - was;
         was = now;
 
-        Iterator<Bullet> it = bullets.iterator();
+        Iterator<BulletView> it = bullets.iterator();
         while (it.hasNext()) {
-            Bullet bullet = it.next();
+            BulletView bullet = it.next();
 
-            double x = bullet.getX() + (delta * Bullet.SPEED / 1000000) * (bullet.isRight() ? 1 : -1);
+            double x = bullet.getX() + (delta * BulletView.SPEED / 1000000) * (bullet.isRight() ? 1 : -1);
             bullet.setX(x);
 
-            if (bullet.getX() < -Bullet.SIZE || bullet.getX() > Main.SCREEN_SIZE) {
-                bullet.remove();
+            if (bullet.getX() < -BulletView.SIZE || bullet.getX() > Main.SCREEN_SIZE) {
+                pane.getChildren().remove(bullet);
                 it.remove();
             }
         }
     }
 
-    public void addBullet(Bullet bullet) {
+    public void addBullet(BulletView bullet) {
         bullets.add(bullet);
     }
 }
 
-class Bullet {
+class BulletView extends ImageView {
     public static final double SIZE = 35;
     public static final double SPEED = .4;
 
     private boolean right;
-    private ImageView view;
-    private Pane pane;
 
-    private static Image image = new Image(Bullet.class.getResource("/china.png").toExternalForm());
+    private static Image image = new Image(BulletView.class.getResource("/china.png").toExternalForm());
 
-    public Bullet(double x, double y, boolean right, Pane pane) {
+    public BulletView(double x, double y, boolean right) {
+        super(image);
         this.right = right;
 
-        view = new ImageView(image);
-        view.setFitHeight(SIZE);
-        view.setFitWidth(SIZE);
-        view.setScaleX(right ? 1 : -1);
+        setFitHeight(SIZE);
+        setFitWidth(SIZE);
+        setScaleX(right ? 1 : -1);
 
-        view.setX(x);
-        view.setY(y);
-
-        this.pane = pane;
-        pane.getChildren().add(view);
-    }
-
-    public void remove() {
-        pane.getChildren().remove(view);
+        setX(x);
+        setY(y);
     }
 
     public boolean isRight() {
         return right;
-    }
-
-    public double getX() {
-        return view.getX();
-    }
-
-    public void setX(double x) {
-        view.setX(x);
-    }
-
-    public double getY() {
-        return view.getY();
-    }
-
-    public void setY(double y) {
-        view.setY(y);
     }
 }
